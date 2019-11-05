857ad4e5e00630f7c8d0d97d3a50a470e645c944
dahan <sdhan>
2019-11-05 17:23:07 +0800

    修改网络模型

--- app/util/dingtalk_util.py
+++ app/util/dingtalk_util.py
@@ -1,5 +1,4 @@
 from flask import g
-import requests
 import json
 
 
@@ -11,22 +10,6 @@ def send_deploy_msg(project, env):
         'msgtype': 'text',
         'text': {
             "content": '{} 正在发布项目：{} 至 {}, 请知悉！'.format(g.user['username'], project, env)
-        }
-    }
-
-    headers = {'Content-Type': 'application/json;charset=UTF-8'}
-
-    requests.post(url=url, headers=headers, data=json.dumps(body))
-
-
-def send_shop_deploy_msg(context):
-    url = 'https://oapi.dingtalk.com/robot/send?' \
-          'access_token=5bc8407ac7e304bef4f2674d01dd7320b2393962b10caa0041bc59d2995a29c7'
-
-    body = {
-        'msgtype': 'text',
-        'text': {
-            "content": context
         }
     }
 
