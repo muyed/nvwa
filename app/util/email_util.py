c577fc7737812486a4c3457870ac2232c742a8fc
svn_user <canofy@163.com>
2019-12-03 15:00:45 +0800

    redis

--- app/util/email_util.py
+++ app/util/email_util.py
@@ -16,24 +16,24 @@ def _format_addr(s):
 
 
 def send(subject, body, to, cc=None):
-  #  msg_id = make_msgid()
-  #  msg = MIMEText(body, 'plain', 'utf-8')
-  #  msg['From'] = _format_addr('女娲<%s>' % from_addr)
-  #  msg['To'] = ','.join(to)
-  #  msg['Subject'] = Header(subject, 'utf-8').encode()
-  #  if cc:
-  #      msg['Cc'] = _format_addr(cc)
-  #      to += cc
+     msg_id = make_msgid()
+     msg = MIMEText(body, 'plain', 'utf-8')
+     msg['From'] = _format_addr('女娲<%s>' % from_addr)
+     msg['To'] = ','.join(to)
+     msg['Subject'] = Header(subject, 'utf-8').encode()
+     if cc:
+         msg['Cc'] = _format_addr(cc)
+         to += cc
 
-  #  msg['Message-ID'] = msg_id
+     msg['Message-ID'] = msg_id
 
-  #  server = smtplib.SMTP(smtp_url, 25)
-  #  server.set_debuglevel(1)
-  # server.login(from_addr, password)
-  #  server.sendmail(from_addr, to, msg.as_string())
-  #  server.quit()
-  #  return msg_id
-  pass
+     server = smtplib.SMTP(smtp_url, 25)
+     server.set_debuglevel(1)
+     server.login(from_addr, password)
+     server.sendmail(from_addr, to, msg.as_string())
+     server.quit()
+     return msg_id
+
 
 
 if __name__ == '__main__':
