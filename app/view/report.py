baf07741f8a346cca450fca6e26db934e0d4c7c1
dahan <sdhan>
2019-12-11 22:14:51 +0800

    sql自动执行

--- app/view/report.py
+++ app/view/report.py
@@ -7,16 +7,16 @@ task_log_service = TaskLogService()
 @app.route("/report/deploy/daily", methods=['GET'])
 def deploy_daily():
     result = task_log_service.group_daily()
-    return json.dumps(result)
+    return json.dumps(result, ensure_ascii=False)
 
 
 @app.route("/report/deploy/pre", methods=['GET'])
 def deploy_pre():
     result = task_log_service.group_pre()
-    return json.dumps(result)
+    return json.dumps(result, ensure_ascii=False)
 
 
 @app.route("/report/deploy/prod", methods=['GET'])
 def deploy_prod():
     result = task_log_service.group_prod()
-    return json.dumps(result)
+    return json.dumps(result, ensure_ascii=False)
