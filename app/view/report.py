afc298b9617bf05eb47e0bd419ab30b4b5b14d15
dahan <sdhan>
2019-12-14 11:42:05 +0800

    sql自动执行

--- app/view/report.py
+++ app/view/report.py
@@ -1,5 +1,6 @@
 from .login import *
-from ..util import db_exec_util, ras_util
+from ..util import db_exec_util
+from flask import Response
 import json
 
 task_log_service = TaskLogService()
@@ -10,31 +11,31 @@ db_info_server = DbInfoService()
 @app.route("/report/deploy/daily", methods=['GET'])
 def deploy_daily():
     result = task_log_service.group_daily()
-    return json.dumps(result, ensure_ascii=False)
+    return Response(json.dumps(result, ensure_ascii=False), mimetype='application/json')
 
 
 @app.route("/report/deploy/pre", methods=['GET'])
 def deploy_pre():
     result = task_log_service.group_pre()
-    return json.dumps(result, ensure_ascii=False)
+    return Response(json.dumps(result, ensure_ascii=False), mimetype='application/json')
 
 
 @app.route("/report/deploy/prod", methods=['GET'])
 def deploy_prod():
     result = task_log_service.group_prod()
-    return json.dumps(result, ensure_ascii=False)
+    return Response(json.dumps(result, ensure_ascii=False), mimetype='application/json')
 
 
 @app.route("/report/sql/db", methods=['GET'])
 def sql_by_db():
     result = sql_approval_service.group_by_db()
-    return json.dumps(result, ensure_ascii=False)
+    return Response(json.dumps(result, ensure_ascii=False), mimetype='application/json')
 
 
 @app.route("/report/sql/promoter", methods=['GET'])
 def sql_by_promoter():
     result = sql_approval_service.group_by_promoter()
-    return json.dumps(result, ensure_ascii=False)
+    return Response(json.dumps(result, ensure_ascii=False), mimetype='application/json')
 
 
 @app.route("/report/db/information", methods=['GET'])
@@ -43,7 +44,7 @@ def db_information():
     params = [[getattr(i, 'db_url'), getattr(i, 'username'), getattr(i, 'password'), getattr(i, 'db_name')]
               for i in infos]
     result = db_exec_util.information(params)
-    return json.dumps(result, ensure_ascii=False)
+    return Response(json.dumps(result, ensure_ascii=False), mimetype='application/json')
 
 
 
